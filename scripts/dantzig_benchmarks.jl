1b7824b81f83b8252273193c4c058d5b08a8e332
Andrew Zheng <atz@dhcp-18-189-1-12.dyn.mit.edu>
2017-10-15 13:12:04 -0400

    Working benchmark code

--- scripts/dantzig_benchmarks.jl
+++ scripts/dantzig_benchmarks.jl
@@ -1,33 +1,53 @@
-using DataFrames
+using DataFrames, ProgressMeter
 
-ns = 2 .^ (7:12)
-ps = 2 .^ (7:12)
+include("../utils.jl")
+include("../dantzig.jl")
+
+ns = 2 .^ (7:11)
+ps = 2 .^ (7:11)
 rhos = 0.0:0.2:0.6
 delta_pcts = 0.2:0.2:0.8
 
 
 function run_benchmark(n, p, rho)
+    info("*** Running benchmark with n=$n, p=$p, rho=$rho ***")
     X, y, _ = generate_regression_example(n, p, 0.2, rho = rho)
-    return vcat([run_instance(X, y, pct, colgen, congen)
+    return vcat([run_instance(X, y, pct, rho, colgen, congen)
                  for pct in delta_pcts
                  for colgen in [false, true]
                  for congen in [false, true]]...)
 end
 
 
-function run_instance(X, y, pct, colgen, congen)
+function run_instance(X, y, pct, rho, colgen, congen)
     n, p = size(X)
     max_delta = maximum(abs.(X'y))
-     _, seconds = @timed DantzigLP.dantzig_lp(y, X, max_delta * pct,
-                                              column_generation = colgen,
-                                              constraint_generation = congen)
-    return DataFrame(n = n, p = p, delta_pct = pct,
-                     colgen = colgen, congen = congen, seconds = seconds)
+    _, _, diagnostics = DantzigLP.dantzig_lp(y, X, max_delta * pct,
+                                             column_generation = colgen,
+                                             constraint_generation = congen,
+                                             return_diagnostics = true)
+    params_df = DataFrame(n = n, p = p, delta_pct = pct, rho = rho,
+                          colgen = colgen, congen = congen)
+    results_df = struct2df([diagnostics])
+    return hcat(params_df, results_df)
 end
 
-results = vcat([run_benchmark(n, p, rho)
-                for n in ns for p in ps for rho in rhos]...)
+# Benchmark vs. other column / constraint generation settings
+params = [(n, p, rho) for n in ns for p in ps for rho in rhos]
+results = []
+@showprogress for param in params
+    push!(results, run_benchmark(param...))
+end
+results_df = vcat(results...)
+writetable("results.csv", results_df)
 
 
-run_instance(X, y, 0.8, true, true)
+n = 5000
+p = 50000
+X, y, _ = generate_regression_example(n, p, 0.2)
+results = run_instance(X, y, 0.8, 0.0, true, true)
+
+
+
+
 
