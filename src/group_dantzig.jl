d522e857eb1fc2a589e472d4156ad585f58196d4
Andy Zheng <azheng92@gmail.com>
2018-03-11 16:26:15 -0400

    Broke fista into separate extensible function

--- src/group_dantzig.jl
+++ src/group_dantzig.jl
@@ -208,73 +208,48 @@ end
 
 # Group Lasso
 # ==============================================================================
-"FISTA solver for the L1/L∞ Group Lasso."
-function group_lasso(X, y, g, λ; tol=1e-6, maxiter=1e6, L=nothing,
-                     line_search=false, line_search_factor=0.5,
-                     verbose=false)
-    n, p = size(X)
-    α = 0 # Acceleration parameter
-    prox_β = spzeros(p)
-    β = spzeros(p)
-
-    L = ifelse(L == nothing, norm(X, 2) .^ 2, L)
-    obj = group_lasso_objective(X, y, g, λ, β)
-    vinfo(msg) = verbose_info(verbose, msg)
-
-    is_converged = false
-
-    if verbose
-        progressbar = Progress(Int(maxiter))
-    end
-
-    for i in 1:maxiter
-        Xᵀr = transpose(X) * (y - X * β)
-        prox_β_new = prox(g, β, λ, Xᵀr, L)
-
-        # Acceleration step
-        α_new = (1 + sqrt(1 + 4 * α ^ 2)) / 2
-        γ = (1 - α) / α_new
-        β_new = (1 - γ) * prox_β_new + γ * prox_β
-
-        obj_new = group_lasso_objective(X, y, g, λ, β_new)
-        if abs(obj_new - obj) ≤ tol && i > 20
-            # Case 1: Termination condition reached; return.
-            is_converged = true
-            break
-        elseif obj_new > obj && line_search
-            # Case 2: Gradient step makes objective worse; shrink step size and
-            # retry.
-            L = L / line_search_factor
-            vinfo(@sprintf("Iteration %d: Reducing step_size to %.8f.",
-                          i, L))
-        else
-            # Case 3: Gradient step improves objective; set new starting point
-            # and continue.
-            α = α_new
-            prox_β = prox_β_new
-            β = β_new
-            obj = obj_new
-        end
-
-        if verbose
-            ProgressMeter.next!(progressbar;
-                                showvalues = [(:iter, i), (:obj, obj)])
-        end
-    end
-
-    if is_converged
-        vinfo("Solution converged.")
-    else
-        vinfo("Max iterations reached.")
-    end
-
-    return sparse(β)
+type GroupLassoProblem
+    X
+    y
+    g
+    λ
 end
 
 
-function group_lasso_objective(X, y, g, λ, β)
+function group_lasso(X, y, g, λ; args...)
+    problem = GroupLassoProblem(X, y, g, λ)
+    L = norm(X, 2) .^ 2
+    return FISTA(problem, L; args...)
+end
+
+
+function fista_objective(problem::GroupLassoProblem, β)
+    @unpack X, y, g, λ = problem
     group_norms = [norm(βg, Inf) for (_, βg) in split_by(β, g)]
     return 0.5 * norm(y - X * β, 2) ^ 2 + λ * norm(group_norms, 1)
+end
+
+
+function prox(problem::GroupLassoProblem, β, L)
+    @unpack X, y, g, λ = problem
+    ∇f = - (X') * (y - X * β)
+    a = β .- (1 / L) * ∇f
+    ag = split_by(a, g)
+
+    βg_new = pmap(x -> (x[1], prox_group(x[2], λ, L)), ag)
+    β_new = vcat_by(βg_new, g)
+    return β_new
+end
+
+
+"""
+Solve the proximal step for a group by projecting ag onto the L1 ball, which
+yields an optimal dual solution.
+"""
+function prox_group(ag, λ, L)
+    λLu = L1_projection(ag, z = λ / L)
+    βg = ag - λLu
+    return βg
 end
 
 
@@ -292,26 +267,7 @@ function vcat_by(xs, by)
 end
 
 
-function prox(g, β, λ, Xᵀr, L)
-    a = β .+ (1 / L) * Xᵀr
-    ag = split_by(a, g)
-
-    βg_new = pmap(x -> (x[1], prox_group(x[2], λ, L)), ag)
-    β_new = vcat_by(βg_new, g)
-    return β_new
-end
-
-"""
-Solve the proximal step for a group by projecting ag onto the L1 ball, which
-yields an optimal dual solution.
-"""
-function prox_group(ag, λ, L)
-    λLu = L1_projection(ag, z = λ / L)
-    βg = ag - λLu
-    return βg
-end
-
-
+"Solve L1 projection using gurobi; for testing purposes."
 function prox_group_gurobi(ag, λ, L)
     pg = length(ag)
     solver = GurobiSolver(OutputFlag=0)
