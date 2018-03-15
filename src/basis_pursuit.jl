746ac1395f9a0462b16f342f4a89ca6d0ce97690
Andy Zheng <azheng92@gmail.com>
2018-03-15 19:30:14 -0400

    Implemented lambda search

--- src/basis_pursuit.jl
+++ src/basis_pursuit.jl
@@ -28,16 +28,25 @@ function initialize_constraints!(model::BasisPursuitModel, λ)
 end
 
 
-function basis_pursuit_initializer(X, y)
+function basis_pursuit_initializer(X, y, λ; args...)
     n, p = size(X)
-    lasso = fit(LassoPath, X, y, maxncoef=p, standardize=false, intercept=false)
+    lasso = fit(LassoPath, X, y, λ=λ / n,
+                maxncoef=p, standardize=false, intercept=false; args...)
     lasso_p = [norm(lasso.coefs[:, i], 0) for i in 1:length(lasso.λ)]
-    coef_idx = minimum(find(lasso_p .≥ n))
-    return sparse(lasso.coefs[:, coef_idx])
+    return sparse(lasso.coefs[:, end])
 end
 
 
-function basis_pursuit(X, y; args...)
+function log_range(minx, maxx, n)
+    factor = exp((1 / n) * (log(maxx) - log(minx)))
+    return minx .* (factor .^ collect(1:n))
+end
+
+
+function basis_pursuit(X, y;
+                       λ_init=1, λ_min=1e-6, nλ=50,
+                       initializer_args=[], args...)
+
     args_dict = Dict(args)
     verbose = get(args_dict, :verbose, false)
     vinfo(msg) = verbose_info(verbose, msg)
@@ -48,18 +57,49 @@ function basis_pursuit(X, y; args...)
     vinfo(@sprintf("Initial model completed in %.2f seconds.",
                    construction_secs))
 
-    initializer_secs = @elapsed initial_soln = basis_pursuit_initializer(X, y)
-    vinfo(@sprintf("Initialization finished in %.2f seconds. L₀(β₀) = %d.",
-                   initializer_secs, norm(initial_soln, 0)))
+    λ_max = norm(X'y, Inf)
+    λ = λ_init
+    is_solved = false
 
-    β, diagnostics = solve_dantzig_lp!(model, 0, initial_soln; args...)
-    return β, model, diagnostics
+    β = nothing
+    model_cp = nothing
+    diagnostics = nothing
+
+    while λ ≥ λ_min && !is_solved
+        try
+            initializer_secs = @elapsed initial_soln =
+                basis_pursuit_initializer(
+                    X, y, log_range(λ_max, λ, nλ);
+                    initializer_args...)
+            vinfo(@sprintf(
+                "Initialization finished in %.2f seconds. L₀(β₀) = %d.",
+                initializer_secs, norm(initial_soln, 0)))
+            model_cp = deepcopy(model)
+            β, diagnostics =
+                solve_dantzig_lp!(model_cp, 0, initial_soln; args...)
+            is_solved = true
+        catch e
+            if isa(e, InfeasibilityError)
+                λ = λ / 2
+                vinfo(@sprintf("Infeasible model; reducing λ to %.6f", λ))
+            else
+                throw(e)
+            end
+        end
+    end
+
+    if !is_solved
+        error("No feasible λ found.")
+    end
+
+    return β, model_cp, diagnostics
 end
 
 
-function baseline_basis_pursuit(X, y; verbose=true)
+function baseline_basis_pursuit(X, y; args...)
     n, p = size(X)
-    model = Model(solver = GurobiSolver(OutputFlag = ifelse(verbose, 1, 0)))
+    solver = construct_solver(; args...)
+    model = Model(solver=solver)
 
     @variables model begin
         β⁺[1:p] ≥ 0
@@ -74,4 +114,11 @@ function baseline_basis_pursuit(X, y; verbose=true)
 
     solve(model)
     return model, sparse(getvalue(β⁺) - getvalue(β⁻))
+end
+
+
+function basis_pursuit_example(n, p, density)
+    X, _, β = regression_example(n, p, density)
+    y = X * β
+    return X, y, sparse(β)
 end
