ff50045c197ad66605a32aa2c1ca7e31671d112d
Andy Zheng <azheng92@gmail.com>
2019-08-08 20:47:12 -0400

    Additonal exports

--- src/DantzigLP.jl
+++ src/DantzigLP.jl
@@ -4,7 +4,9 @@ module DantzigLP
 
 export baseline_dantzig, dantzig_lp, regression_example,
     baseline_group_dantzig, group_dantzig, group_lasso, group_dantzig_example,
-    dantzig_tf, tf_example
+    baseline_dantzig_tf, dantzig_tf, tf_example,
+    baseline_basis_pursuit, basis_pursuit, basis_pursuit_example,
+    baseline_dantzig_fused_regression, dantzig_fused_regression, fused_regression_example
 
 include("utils.jl")
 include("fista.jl")
