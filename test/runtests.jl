c5b4bad386ee2fb1bd51fd130a46ebe7ddda4ca7
Andy Zheng <azheng92@gmail.com>
2019-08-09 14:13:22 -0400

    Updated Test Cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,75 +2,37 @@ using Base.Test, JuMP
 
 using DantzigLP
 
-ε = 1e-6
 
-function test_get_reduced_costs(model::DantzigLP.DantzigModel)
-    actual_costs = vcat(getdual(model.pos_betas),
-                        getdual(model.neg_betas))
-    test_pos_costs, test_neg_costs = DantzigLP.get_reduced_costs(model)
-    test_costs = vcat(test_pos_costs[model.pos_beta_indices],
-                      test_neg_costs[model.neg_beta_indices])
-    @test actual_costs ≈ test_costs atol=ε
-end
+# Test Dantzig Selector
+# ------------------------------------------------------------------------------
+X, y, actual = regression_example(100, 200, 0.2)
+β, model, log_fast = dantzig_lp(X, y, 1)
+β_colgen, _, _ = dantzig_lp(X, y, 1; constraint_generation=false)
+β_congen, _, _ = dantzig_lp(X, y, 1; column_generation=false)
+bl_model, β_baseline = baseline_dantzig(X, y, 1)
+@test isapprox(β, β_baseline)
+@test isapprox(β, β_colgen)
+@test isapprox(β, β_congen)
+@test isapprox(getobjectivevalue(model.gurobi_model),
+               getobjectivevalue(bl_model))
 
+# Test Basis Pursuit
+# ------------------------------------------------------------------------------
+X, y, actual = basis_pursuit_example(100, 200, 0.2)
+β, model, log_fast = basis_pursuit(X, y; max_columns=40, verbose=false)
+bl_model, β_baseline = baseline_basis_pursuit(X, y)
+@test isapprox(β, β_baseline)
+@test isapprox(getobjectivevalue(model.gurobi_model),
+               getobjectivevalue(bl_model))
 
-function test_optimality(model::DantzigLP.DantzigModel, β, baseline_β)
-    @test objective(model, β) ≈ objective(model, baseline_β) atol=ε
-end
-
-
-function objective(model::DantzigLP.DantzigModel, β)
-    return norm(β, 1)
-end
-
-
-function objective(model::DantzigLP.GroupDantzigModel, β)
-    return sum([norm(βs, Inf) for (group, βs) in DantzigLP.split_by(β, model.groups)])
-end
-
-
-function run_group_dantzig(; args...)
-    # Generate problem
-    n = 100
-    p = 1000
-    k = Int(ceil(p / 10))
-    λ = 50
-    X, y, g, true_β = group_dantzig_example(n, p, k, 0.2)
-
-    # Get baseline
-    baseline_β, _ = baseline_group_dantzig(X, y, g, λ)
-
-    # Get model
-    β, model, _ = group_dantzig(X, y, g, λ; args...)
-
-    return (X, y, g, λ), model, β, baseline_β
-end
-
-
-function test_feasibility(model::DantzigLP.GroupDantzigModel, problem, β)
-    X, y, g, λ = problem
-    Xtr = (X') * (y - X * β)
-    group_norms = [norm(Xtr[find(g .== group)], 1) for group in 1:maximum(g)]
-    @test norm(group_norms, Inf) - λ ≈ 0 atol=ε
-end
-
-
-# problems = [run_dantzig_lp, run_group_dantzig, run_dantzig_tf]
-algos = [run_group_dantzig]
-scenarios = [[(:column_generation, false), (:constraint_generation, false)],
-             [(:column_generation, true), (:constraint_generation, false)],
-             [(:column_generation, false), (:constraint_generation, true)],
-             [(:column_generation, true), (:constraint_generation, true)]]
-
-
-for algo in algos
-    for scenario in scenarios
-        problem, model, β, baseline_β = algo(; scenario...)
-
-        @testset "Model Correctness" begin
-            test_get_reduced_costs(model)
-            test_feasibility(model, problem, β)
-            test_optimality(model, β, baseline_β)
-        end
-    end
-end
+# Test Dantzig trend filtering
+# ------------------------------------------------------------------------------
+noisy, clean, a = DantzigLP.trend_filtering_example(100, 0, 20)
+β_baseline, bl_model = DantzigLP.baseline_dantzig_tf(
+    noisy, 1, 0; timeout=300, verbose=true)
+β, model = dantzig_tf(noisy, 1, 0;
+                      column_generation=true,
+                      constraint_generation=true)
+# Solutions are sometimes different, but objective values should be the same.
+@test isapprox(getobjectivevalue(model.gurobi_model),
+               getobjectivevalue(bl_model.gurobi_model))
