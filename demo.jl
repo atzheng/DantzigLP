17e6208367ef041d38bceebfd3d94c2aca8ebe78
Andy Zheng <azheng92@gmail.com>
2019-08-12 21:30:33 -0400

    Updated demo

--- demo.jl
+++ demo.jl
@@ -3,8 +3,8 @@ using DantzigLP
 
 # Dantzig Selector
 # ==============================================================================
-# generates dataset 
-# y_{100} is response, X_{100 \times 200}=model matrix, 
+# generates dataset
+# y_{100} is response, X_{100 \times 200}=model matrix,
 # actual=true regression coefficients with 0.2 proportion of nonzero entries
 X, y, actual = regression_example(100, 200, 0.2)
 
@@ -27,11 +27,10 @@ X, y, actual = basis_pursuit_example(100, 200, 0.2)
 # run basis pursuit with column generation enabled
 β, model, log_fast = basis_pursuit(X, y)
 
-# run basis pursuit with column generation disabled 
-# (here constraint generation is vacuous) [Andy, can we remove constraint_generation= option?]
+# run basis pursuit with column generation disabled
+# (here constraint generation is vacuous)
 β, model, log_fast = basis_pursuit(X, y;
-                                   column_generation=false,
-                                   constraint_generation=false)
+                                   column_generation=false)
 
 # Fused Dantzig Regression
 # ==============================================================================
@@ -42,10 +41,9 @@ noisy, clean, a = DantzigLP.trend_filtering_example(100, 0, 20)
 # Run Dantzig Selector version of fused lasso
 # y=signal,  0.1=value of regularization parameter, k=0 specifies fused lasso.
 # the following used both column and constraint generation
-β, model = dantzig_tf(y, 0.1, 20)
+β, model = dantzig_tf(y, 1, 0)
 
 # the following disables both column and constraint generation
-β, model = dantzig_tf(y, 0.1, 20;
+β, model = dantzig_tf(y, 1, 0;
                       column_generation=false,
                       constraint_generation=false)
-
